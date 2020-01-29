36bede3f22ce65158cabd4912fc8c37a7a61ca36
DalexKraus <dalex.kraus@gmail.com>
2020-01-29 08:28:36 +0100

    Added md5sum

--- src/at/dalex/grape/toolbox/FileUtil.java
+++ src/at/dalex/grape/toolbox/FileUtil.java
@@ -1,10 +1,30 @@
 package at.dalex.grape.toolbox;
 
-import java.io.File;
+import java.io.*;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class FileUtil {
 
 	public static boolean isFile(File file) {
 		return (file.exists() && !file.isDirectory());
+	}
+
+	public static String md5sum(InputStream input) throws IOException {
+		BufferedInputStream in = new BufferedInputStream(input);
+		try {
+			MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+			DigestInputStream digestInputStream = new DigestInputStream(in, digest);
+			for (; digestInputStream.read() >= 0;) { }
+
+			ByteArrayOutputStream md5out = new ByteArrayOutputStream();
+			md5out.write(digest.digest());
+			return md5out.toString();
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalStateException("MD5 algorithm is not available: " + e);
+		} finally {
+			in.close();
+		}
 	}
 }
